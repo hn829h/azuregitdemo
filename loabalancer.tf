e22efdb5656e6d6251886144256ed853c48033df
hari <harish.nidadavolu87@gmail.com>
2020-01-12 11:26:02 +0530

    new code

--- loabalancer.tf
+++ loabalancer.tf
@@ -9,11 +9,6 @@ resource "azurerm_lb" "main" {
   name                = "${var.prefix}-LB"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
-
-  frontend_ip_configuration {
-    name                 = "primary"
-    public_ip_address_id = "${azurerm_public_ip.main.id}"
-  }
 }
 
 resource "azurerm_lb_backend_address_pool" "main" {
@@ -22,20 +17,9 @@ resource "azurerm_lb_backend_address_pool" "main" {
   name                = "acctestpool"
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "main-nic"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.main-SN.id}"
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
-  network_interface_id    =  [element(azurerm_network_interface.main-NI.*.id, count.index)]
+  count                 = var.node_count
+  network_interface_id    =  "${azurerm_network_interface.main-NI.${count.index}.id}
   ip_configuration_name   = "testconfiguration1"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
 }
